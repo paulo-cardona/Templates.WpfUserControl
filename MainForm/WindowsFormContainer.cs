c6fb13ebfafeb6af179dde708394a66c445c34a6
Paulo <paulocardona@outlook.com>
2015-11-19 15:30:58 -0500

    1) Added WindowFormsContainer to demonstrate how to host a WPF View in Winforms. 2) Added a .gitignore file

---
+++ MainForm/WindowsFormContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace MainForm
+{
+    public partial class WindowsFormContainer : Form
+    {
+        public readonly ElementHost _elementHost;
+        private Views.MainWindow _mainView;
+
+        public WindowsFormContainer()
+        {
+            InitializeComponent();
+
+            _mainView = new Views.MainWindow(); //Create Instance of WPF View
+            _elementHost = new ElementHost();   //Create ElementHost (System.Windows.Forms.Integration)
+            _elementHost.Child = _mainView;     //Set our WPF View as elementhost.child.
+            this.Controls.Add(_elementHost);    //Add elementhost to form controls collection.
+            FormatWindow();                     //I haven't found a better way to do this yet.
+        }
+
+        public void FormatWindow()
+        {
+            //Set all Container window properties here (ie. Size/Title)
+            //_elementHost.Width = xxx;
+            //_elementHost.Height = xxx;
+            //Width = _elementHost.Width + 20;
+            //Height = _elementHost.Height + 40;
+        }
+    }
+}
